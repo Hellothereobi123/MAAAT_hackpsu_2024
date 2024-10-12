23e8ef63ff98665bcb81995b1632884ce0dc2c1a
Hellothereobi123 <91630736+Hellothereobi123@users.noreply.github.com>
2024-10-12 16:22:41 -0400

    render another file through flask

---
+++ app.py
@@ -0,0 +1,16 @@
+# import flast module
+from flask import Flask, render_template
+
+# instance of flask application
+app = Flask(__name__)
+
+# home route that returns below text when root url is accessed
+@app.route("/")
+def hello_world():
+    return "<h1>Hello, World!</h1>"
+@app.route("/lols")
+def hello_worldo():
+    return render_template('./temp_file.html')
+
+if __name__ == '__main__':  
+   app.run()
